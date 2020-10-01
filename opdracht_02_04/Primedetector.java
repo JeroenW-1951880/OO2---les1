76471154852fe6f03e5b0d439c71a0d2f4b99e1b
Jeroen <jeroen.weltens@student.uhasselt.be>
2020-10-01 16:08:12 +0200

    opdracht-02-04 comments added

--- opdracht_02_04/Primedetector.java
+++ opdracht_02_04/Primedetector.java
@@ -1,7 +1,14 @@
 package com.jetbrains;
 
+/** Program that checks numbers for being prime
+ * @author Jeroen Weltens
+ */
 public class Primedetector {
 
+    /** the main contains all functionality
+     *
+     * it checks the first 100 numbers for being prime
+     */
     public static void main(String[] args) {
         for (int i = 2; i < 101; ++i) {
             boolean prime = true;
